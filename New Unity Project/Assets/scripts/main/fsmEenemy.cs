bcddaa1b3aaef838ecc3722e82dcaf1a126010c8
Park Du Hwan <pdh930318@gmail.com>
2020-07-10 09:00:04 +0900

    add fsm

--- New Unity Project/Assets/scripts/main/fsmEenemy.cs
+++ New Unity Project/Assets/scripts/main/fsmEenemy.cs
@@ -2,158 +2,210 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public interface IState
-{
-    void OperateEnter();
-    void OperateUpdate();
-    void OperateExit();
-}
 
-public class StateMachine
-{
-    //현재 상태를 담는 프로퍼티.
-    public IState CurrentState { get; private set; }
 
-    //기본 상태를 생성시에 설정하게 생성자 만들기.
-    public StateMachine(IState defaultState)
-    {
-        CurrentState = defaultState;
-    }
-
-    //외부에서 현재상태를 바꿔주는 부분.
-    public void SetState(IState state)
-    {
-        //같은 행동을 연이어서 세팅하지 못하도록 예외처리.
-        //예를 들어, 지금 점프중인데 또 점프를 하는 무한점프 버그를 예방할수도 있다.
-        if (CurrentState == state)
-        {
-            Debug.Log("현재 이미 해당 상태입니다.");
-            return;
-        }
-
-        //상태가 바뀌기 전에, 이전 상태의 Exit를 호출한다.
-        CurrentState.OperateExit();
-
-        //상태 교체.
-        CurrentState = state;
-
-        //새 상태의 Enter를 호출한다.
-        CurrentState.OperateEnter();
-    }
-
-    //매프레임마다 호출되는 함수.
-    public void DoOperateUpdate()
-    {
-        CurrentState.OperateUpdate();
-    }
-}
-
-public class stateidle : IState
-{
-    Behavior behavior;
-    public void OperateEnter()
-    {
-        
-    }
-
-    public void OperateExit()
-    {
-
-    }
-
-    public void OperateUpdate()
-    {
-
-    }
-}
-public class statepatroll : IState
-{
-    Behavior behavior;
-    public void OperateEnter()
-    {
-        // 가장 근처에있는 웨이포인트를 찾아서 넣는다 근처에 없다면 마지막 기억했던 웨이포인트로 돌아가기
-    }
-
-    public void OperateExit()
-    {
-        // 마지막 웨이포인트 기억하기
-    }
-
-    public void OperateUpdate()
-    {
-        
-        // 웨이포인트 따라가면서 지점도착하면 다음웨이포인트로 넘어간다 
-
-    }
-}
-public class statechase : IState
-{
-    Behavior behavior;
-    public void OperateEnter()
-    {
-        //화들짝놀라는 에니메이션 
-
-    }
-
-    public void OperateExit()
-    {
-        // 아이들 상태로 돌아간다
-
-    }
-
-    public void OperateUpdate()
-    {
-        //약 5초간 쫓아가며 5초이후이거나 일정거리가 멀어진다면 상태에서 나온다.
-
-    }
-}
 public class fsmEenemy : MonoBehaviour
 {   GameObject temp;
     public int waynum;
-    
+    Behavior behavior;
     Vector3 targetpos;
-    private StateMachine stateMachine;
-    private Dictionary<State, IState> dicState = new Dictionary<State, IState>();
-
-    IState idle = new stateidle();
-    IState patrol = new stateidle();
-    IState gotta = new stateidle();
-    IState chase = new stateidle();
-    public enum State //기본상태,정찰,조사,쫓음 4가지 상태
-    {
-        idle,
-        patrol,
-        chase,
-        gotta
-    }
-   
+    Animator ani;
+    Quaternion targetRot;
+    Vector3 dir;
+    Vector3 dirXZ;
     void Start()
     {
-        dicState.Add(State.idle, idle);
-        dicState.Add(State.chase, chase);
-        dicState.Add(State.gotta, gotta);
-        dicState.Add(State.patrol, patrol);
+        
+        waynum = 1;
 
-        //기본상태는 달리기로 설정.
-        stateMachine = new StateMachine(idle);
+    }
+    private void Awake()
+    {
+        ChangeState(State.idle);
+        temp = GameObject.Find("waypoint").transform.GetChild(1).gameObject;
 
-        waynum = 0;
-        temp = GameObject.FindWithTag("Waypoint").transform.GetChild(waynum).gameObject;
-
+        ani = GetComponent<Animator>();
     }
 
     public void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
-            stateMachine.SetState(dicState[State.gotta]);
+        }
+    }
+    //상태머신
+    public enum State
+    {
+        idle = 0,
+        chase = 1,
+        gotta= 2 ,
+        patroll = 3
+
+    }
+    public int prestate;
+    public State state;
+
+    private void ChangeState(State state)
+    {
+        //스테이트에서 나가기전에 마지막으로 실행되는 Exit()함수;
+        switch (this.state)
+        {
+            case State.idle:
+                idleExit();
+                break;
+            case State.chase:
+                chaseExit();
+                break;
+            case State.gotta:
+                gottaExit();
+                break;
+            case State.patroll:
+                patrollExit();
+                break;
+    
+        }
+
+        this.state = state;
+
+        //스테이트에서 들어가고나서 처음으로 실행되는 Enter()함수;
+        switch (state)
+        {
+            case State.idle:
+                idleEnter();
+                break;
+            case State.chase:
+                chaseEnter();
+                break;
+            case State.gotta:
+                gottaEnter();
+                break;
+            case State.patroll:
+                patrollEnter();
+                break;
+
         }
     }
 
+    private void idleEnter()
+    {
+
+    }
+    private void idle()
+    {
+        ChangeState(State.patroll);
+    }
+    private void idleTrigger(Collider other)
+    {
+
+    }
+    private void idleTriggerStay(Collider other)
+    {
+
+    }
+    private void idleExit()
+    {
+
+    }
+
+
+    private void chaseEnter()
+    {
+    }
+    private void chase()
+    {
+    }
+    private void chaseTrigger(Collider other)
+    {
+    }
+    private void chaseExit()
+    {
+    }
+
+
+
+
+    private void gottaEnter()
+    {
+    }
+    private void gotta()
+    {
+    }
+    private void gottaTrigger(Collider other)
+    {
+    }
+    private void gottaExit()
+    {
+    }
+
+
+
+
+    private void patrollEnter()
+    {
+        ani.SetTrigger("idletowalk");
+    }
+    private void patroll()
+    {
+       float dis = Vector3.Distance(transform.position, temp.transform.position);
+        if (dis > 0.01)
+        {
+            Vector3 targetpos = temp.transform.position;
+           
+          
+                // 캐릭터를 이동시킨다.
+                targetpos.y = transform.position.y;
+                transform.localPosition = Vector3.MoveTowards(transform.position, targetpos, 2.0f * Time.deltaTime);
+
+             dir = targetpos - transform.position;
+             dirXZ = new Vector3(dir.x, 0, dir.z);
+            if (dirXZ.magnitude > 0)
+              targetRot = Quaternion.LookRotation(dirXZ);
+            if (dirXZ.x < 0.001 || dirXZ.z < 0.001)
+            {
+                dirXZ = new Vector3(0, 0, 0);
+            }
+            if (Vector3.Distance(transform.position, targetpos) > 0.2f)
+            {
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRot, 550.0f * Time.deltaTime);
+
+            }
+        }
+        else { waynum += 1; }
+        if(waynum > 14)
+        {
+            waynum = 1;
+        }
+    }
+    private void patrollTrigger(Collider other)
+    {
+    }
+    private void patrollExit()
+    {
+       
+    }
+
+
+
     void Update()
     {
+        //스테이트상태에따라 업데이트
+        switch (state)
+        {
+            case State.idle:
+                idle();
+                break;
+            case State.chase:
+                chase();
+                break;
+            case State.gotta:
+                gotta();
+                break;
+            case State.patroll:
+                patroll();
+                break;
+
+        }
         //매초실행되야하는거 불러오기
-        stateMachine.DoOperateUpdate();
-        
+
     }
 }
